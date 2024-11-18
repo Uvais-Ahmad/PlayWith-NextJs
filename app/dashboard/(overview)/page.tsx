0e7f86bd94822cfaebb6c795a31e581775ae5f41
Uvais Ahmad <uvss21@gmail.com>
2024-11-18 23:11:35 +0530

    [Update ] Add Dashboard button on Homepage

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -8,9 +8,6 @@ import React, { Suspense } from 'react'
 
 
 export default async function Page() {
-    // const revenue = await fetchRevenue();
-    const latestInvoices = await fetchLatestInvoices();
-    const {numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices} = await fetchCardData();
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl `}>

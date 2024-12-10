6298040b156939fa8c4b8b959f9687018c54d546
Uvais Ahmad <uvss21@gmail.com>
2024-12-10 23:30:06 +0530

    [Remove Comments]

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -23,7 +23,6 @@ export default async function CardWrapper() {
   } = await fetchCardData();
   return (
     <div className='flex justify-between'>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
       <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
